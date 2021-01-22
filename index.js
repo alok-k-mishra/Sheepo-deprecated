75cb995d260fe6b4ae32df8f2b25d6e4392f35c4
Recode <recodeexistance@gmail.com>
2021-01-22 01:39:38 +0000

    Added helper functions for error handling and permissions for abstraction.  needs tested.  also tidied up code architecture and replaces nested conditionals with succinct switch statements.

--- index.js
+++ index.js
@@ -1,175 +1,200 @@
+const commandFiles = ['ban', 'bday', 'bonk', 'clearc', 'doubt', 'help', 'helpmember', 'helpmod', 'hug', 'kick', 'mute', 'pat', 'ping', 'playlist', 'purge', 'slap', 'slowmode', 'tempmute', 'unmute', 'youtube']; 
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
 const GuildMemberManager = require("discord.js");
-
-
-const prefix = '.';
-
-const fs = require('fs');
-
+const prefix = process.env.PREFIX;
+require("dotenv").config();
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN; 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+commandFiles.forEach(filename => {
+  try {
+  const command = require(`./commands/${filename}.js`);
+    client.commands.set(command.name, command);
+    console.log(`${filename} was loaded and set.`); 
+} catch {
+  console.log(`Oops something went wrong! ${filename} did not load!`);
+  }
+}); 
 
-for(const file of commandFiles){
-  console.log("Error")
-  const command = require(`./commands/${file}`);
-
-  client.commands.set(command.name, command);
-}
 client.once('ready', () =>{
   console.log('SheepyMod is Online!');
-  client.user.setActivity('Sheepy Server', { type: 'WATCHING'}).catch();
+  try {
+    client.user.setActivity('Sheepy Server', { type: 'WATCHING'}) 
+  } catch {
+    console.log(`Error at client ready, check setActivity!`); 
+    return;  
+  }
 });
 
 client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if (message.channel.type !== 'dm') {
-
-    
-    
-    if(command === 'purge'){
-      if (message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("BAN_MEMBERS")){
-        client.commands.get('purge').execute(message, args, Discord).catch(() => {;
-  
-          message.reply("Hey you ain't a mod or admin >:(");
-          })
-      }
+  if(!message.content.startsWith(prefix) || message.author.bot) {
+    return;
+  } else {
+    if (message.channel.type !== 'dm') {
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+        
+        // Below follows a switch statement, to remove a lot of the clutter, and improve the efficiency of the code.  I've also group together certain commands that have aliases, added try / catch blocks too although 
+        // I have to work on these being added.  I've also added a helper function and used that to test user permissions, abstracting away all redudant code.  
+        switch(command) {
+          case 'ban':  
+          if (userHasPermissions(message.member)){
+            try {
+              client.commands.get('ban').execute(message, args, Discord); 
+            } catch{
+                addError(1, message.member); 
+            }
+          }
+          break;       
+        
+          case 'bday': 
+            client.commands.get('bday').execute(message, args, Discord);
+          break;
+        
+          case 'bonk':
+            client.commands.get('bonk').execute(message, args, Discord);
+          break;
+        
+          case 'clear':
+            if(userHasPermissions(message.member)) {
+              try {
+                client.commands.get('clearc').execute(message, args, Discord);
+                console.log(`tidying up...`) 
+              } catch {
+                addError(1, message.member); 
+              }
+            }
+          break;
+          
+          case 'doubt':
+            client.commands.get('doubt').execute(message, args, Discord);
+          break;
+        
+          case 'help':
+            client.commands.get('help').execute(message, args, Discord);
+          break;
+        
+          case 'helpmember':
+            client.commands.get('helpmember').execute(message, args, Discord);
+          break;
+        
+          case 'helpmod':
+            client.commands.get('helpmod').execute(message, args, Discord);
+          break;
+        
+          case 'hug':
+            client.commands.get('hug').execute(message, args, Discord);
+          break;
+        
+          case 'kick':
+            if (userHasPermissions(message.member)){
+              try {
+                client.commands.get('kick').execute(message, args, Discord)
+              } catch {
+                addError(1, message.member); 
+                }
+            }
+          break;
+        
+          case 'mute':
+            if (userHasPermissions(message.member)){
+              client.commands.get('mute').execute(message, args, Discord).catch(() => {;
+        
+                addError(1, message.member); 
+                })
+            }
+          break;
+        
+          case 'pat':
+            client.commands.get('pat').execute(message, args, Discord);
+          break;
+        
+          case 'ping':
+            client.commands.get('ping').execute(message, args, Discord);
+          break;
+        
+          case 'playlist':
+          case 'playlists': 
+            client.commands.get('playlist').execute(message, args, Discord);
+          break;
+        
+        
+          case 'purge':
+            if (userHasPermissions(message.member)){
+              client.commands.get('purge').execute(message, args, Discord).catch(() => {;
+        
+                addError(1, message.member); 
+                })
+            }
+          break;
+        
+          case 'slap':
+            client.commands.get('slap').execute(message, args, Discord);
+          break;
+        
+          case 'slowmode':
+          case 'sm': 
+            if (userHasPermissions(message.member)){
+              client.commands.get('slowmode').execute(message, args, Discord).catch(() => {;
+        
+                addError(1, message.member); 
+                })
+            }
+          break;
+        
+          case 'tempmute':
+          case 'tmute': 
+            if (userHasPermissions(message.member)){
+              client.commands.get('tempmute').execute(message, args, Discord).catch(() => {;
+        
+                addError(1, message.member); 
+                })
+            }
+          break;
+        
+          case 'unmute':
+            client.commands.get('unmute').execute(message, args, Discord);
+          break;
+        
+          case 'youtube':
+          case `yt`:
+            client.commands.get('youtube').execute(message, args, Discord);
+          break; 
+        
+          default: 
+          addError(2, message.member); 
+         } 
         }
-   else if(command === 'clear') {
-    if (message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("BAN_MEMBERS")){
-      client.commands.get('clearc').execute(message, args, Discord).catch(() => {;
+      }})
 
-        message.reply("Hey you ain't a mod or admin >:(");
-        })
-    }
+      function userHasPermissions(vMember) {
+        const permsArray = ['KICK_MEMBERS', "MANAGE_MESSAGES", "MANAGE_SERVER", "BAN_MEMBERS"];
+        let bHasPermission = false;   
+        permsArray.forEach(permission => {
+          if(vMember.hasPermission(permission)) { 
+            bHasPermission = true; 
+          }
+        });
+        return bHasPermission;   
+      }
+      
+      function addError(vErrorCode, vMemberName) {
+        let errMessage = "" ;
+        switch(vErrorCode) {
+          case 1: 
+            errMessage = `Hey ${vMemberName} you ain't a mod or admin!  Why are you trying to use this?`;
+          break; 
+          case 2: 
+            errMessage = `Yo!  You gotta use my commands in Sheepy Server, ${vMemberName}`; 
+          break; 
+          default: 
+          errMessage = "Something went wrong!"; 
 
-  } else if(command === 'kick') {
-    if (message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("BAN_MEMBERS")){
-      client.commands.get('kick').execute(message, args, Discord).catch(() => {;
-
-        message.reply("Hey you ain't a mod or admin >:(");
-        })
-    }
-
-  } else if(command === 'ban') {
-    if (message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("BAN_MEMBERS")){
-      client.commands.get('ban').execute(message, args, Discord).catch(() => {;
-
-        message.reply("Hey you ain't a mod or admin >:(");
-        })
-    }
-
-  } else if(command === 'mute') {
-    if (message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("BAN_MEMBERS")){
-      client.commands.get('mute').execute(message, args, Discord).catch(() => {;
-
-        message.reply("Hey you ain't a mod or admin >:(");
-        })
-    }
-
-  } else if(command === 'tempmute') {
-    if (message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("BAN_MEMBERS")){
-      client.commands.get('tempmute').execute(message, args, Discord).catch(() => {;
-
-        message.reply("Hey you ain't a mod or admin >:(");
-        })
-    }
-
-  }else if(command === 'tmute') {
-    if (message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("BAN_MEMBERS")){
-      client.commands.get('tempmute').execute(message, args, Discord).catch(() => {;
-
-        message.reply("Hey you ain't a mod or admin >:(");
-        })
-    }
-
-  } else if(command === 'sm') {
-    if (message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("BAN_MEMBERS")){
-      client.commands.get('slowmode').execute(message, args, Discord).catch(() => {;
-
-        message.reply("Hey you ain't a mod or admin >:(");
-        })
-    }
-  
-  } else if(command === 'slowmode') {
-    if (message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("MANAGE_SERVER") || message.member.hasPermission("BAN_MEMBERS")){
-      client.commands.get('slowmode').execute(message, args, Discord).catch(() => {;
-
-        message.reply("Hey you ain't a mod or admin >:(");
-        })
-    }}
+        };
+        message.reply(errMessage); 
+        return;
+      }
 
 
-
-   if(command === 'help') {
-      client.commands.get('help').execute(message, args, Discord);
-
-  } else if (command === 'helpmember'){
-      client.commands.get('helpmember').execute(message, args, Discord);
-
-  } else if (command === 'helpmod'){
-      client.commands.get('helpmod').execute(message, args, Discord);
-    
-  } else if(command === 'youtube') {
-    client.commands.get('youtube').execute(message, args, Discord);
-
-  } else if(command === 'playlist') {
-    client.commands.get('playlist').execute(message, args, Discord);
-
-  } else if(command === 'playlists') {
-    client.commands.get('playlist').execute(message, args, Discord);
-
-  } else if(command === 'yt') {
-    client.commands.get('youtube').execute(message, args, Discord);
-
-  } else if(command === 'ping') {
-    client.commands.get('ping').execute(message, args, Discord);
-
-  }
-  else if(command === 'bonk') {
-  client.commands.get('bonk').execute(message, args, Discord);
-
-  }
-  else if(command === 'bday') {
-  client.commands.get('bday').execute(message, args, Discord);
-
-  }
-  else if(command === 'hug') {
-  client.commands.get('hug').execute(message, args, Discord);
-
-  } 
-  else if(command === 'unmute') {
-  client.commands.get('unmute').execute(message, args, Discord);
-  }
-  else if(command === 'doubt') {
-  client.commands.get('doubt').execute(message, args, Discord);
-  }
-  else if(command === 'slap') {
-  client.commands.get('slap').execute(message, args, Discord);
-  }
-  else if(command === 'pat') {
-  client.commands.get('pat').execute(message, args, Discord);
-  
-}
-
-  
-  }
-else {
-
-    message.author.send("Hey, you gotta use my commands in sheepy server :)");
-}
-
-});
-
-
-
-
-client.login(process.env.DJS_TOKEN);
+client.login(process.env.DISCORD_TOKEN);  
