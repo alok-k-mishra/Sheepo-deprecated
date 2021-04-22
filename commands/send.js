a544d08e5616e549d3579dfc53d615b32710bd6c
NotKilleration <mail.alokkumarmishra2004@gmail.com>
2021-04-22 09:01:01 +0530

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -1,11 +1,13 @@
+client.guilds.cache.get('426851648309035008')
+guild.channels.cache.get("426851648309035011")
+
 module.exports = {
     name: 'send',
     description: "Secret Sending from dms",
     execute(message, args, Discord) {
         
-        client.guilds.cache.get('426851648309035008')
-        guild.channels.cache.get("426851648309035011")
-        .catch(console.error);
+
+
         var message = args.join(" ").slice(22)
 
 
