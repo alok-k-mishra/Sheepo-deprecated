7ff8d3587aecf3cad4d8ec6da8bb62ca0bbd45e4
NotKilleration <mail.alokkumarmishra2004@gmail.com>
2021-04-22 10:11:01 +0530

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -10,28 +10,22 @@ module.exports = {
         var dmmessage = args.slice(1).join(" ")
        /// console.log(dmmessage)
 
-       const channel1 = guild.channels.cache.get("426851648309035011")
-       const channel2 = guild.channels.cache.get("682270340939776067") 
-       const channel3 = guild.channels.cache.get("765564135798079518")
-       const channel4 = guild.channels.cache.get("455037507734863882")
-       const channel5 = guild.channels.cache.get("464279991463706627")
-
        if(args[1] === 'gen'){
-        channel1.send(dmmessage);
+       const channel = guild.channels.cache.get("426851648309035011")
     }
        if(args[1] === 'pvm'){
-        channel2.send(dmmessage);
+       const channel = guild.channels.cache.get("682270340939776067") 
        }
        if(args[1] === 'selfies'){
-        channel3.send(dmmessage);
+       const channel = guild.channels.cache.get("765564135798079518")
         }
        if(args[1] === 'ap'){
-        channel4.send(dmmessage);
+       const channel = guild.channels.cache.get("455037507734863882")
        }
        if(args[1] === 'intro'){
-        channel5.send(dmmessage);
+       const channel = guild.channels.cache.get("464279991463706627")
        }
 
-
+    channel.send(dmmessage);
 }
 }
