85f62ef7b3b1a77669e29613511d3eca7636774f
NotKilleration <mail.alokkumarmishra2004@gmail.com>
2021-04-22 09:56:05 +0530

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -6,22 +6,30 @@ module.exports = {
         
         const guild = message.client.guilds.cache.get('426851648309035008')
         
-        if(args[1] === 'gen')
-        var channel = guild.channels.cache.get("426851648309035011")
-        if(args[1] === 'pvm')
-        var channel = guild.channels.cache.get("682270340939776067") 
-        if(args[1] === 'selfies')
-        var channel = guild.channels.cache.get("765564135798079518") 
-        if(args[1] === 'ap')
-        var channel = guild.channels.cache.get("455037507734863882") 
-        if(args[1] === 'intro')
-        var channel = guild.channels.cache.get("464279991463706627") 
- 
+
         var dmmessage = args.slice(1).join(" ")
        /// console.log(dmmessage)
 
-        message.channel.send(dmmessage);
-
+       if(args[1] === 'gen'){
+       var channel = guild.channels.cache.get("426851648309035011")
+       message.channel.send(dmmessage)
+    }
+       if(args[1] === 'pvm'){
+       var channel = guild.channels.cache.get("682270340939776067") 
+       message.channel.send(dmmessage)
+       }
+       if(args[1] === 'selfies'){
+       var channel = guild.channels.cache.get("765564135798079518")
+       message.channel.send(dmmessage) 
+        }
+       if(args[1] === 'ap'){
+       var channel = guild.channels.cache.get("455037507734863882") 
+       message.channel.send(dmmessage)
+       }
+       if(args[1] === 'intro'){
+       var channel = guild.channels.cache.get("464279991463706627")
+       message.channel.send(dmmessage) 
+       }
 
 }
 }
