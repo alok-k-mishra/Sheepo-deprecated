f2ad809e3c44d048028d20eee357e0a44563618e
NotKilleration <mail.alokkumarmishra2004@gmail.com>
2021-04-22 09:43:15 +0530

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -17,8 +17,8 @@ module.exports = {
         if(args[1] === 'intro')
         var channel = guild.channels.cache.get("464279991463706627") 
  
-        var dmmessage = args.join(" ").then(console.log(dmmessage)).slice(22)
-        console.log(dmmessage)
+        var dmmessage = args.join(" ").then(console.log('before slicing: ' + dmmessage)).slice(22)
+        console.log('after slicing: '+ dmmessage)
 
    ///     message.channel.send(dmmessage);
 
