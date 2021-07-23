67d0837cff14e2eb4610f97c9926b7c21efdb605
AryunGupta <aryungupta@gmail.com>
2021-07-23 18:43:19 +0530

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -3,8 +3,8 @@ module.exports = {
     description: "Help Command",
     execute(message, args, Discord) {
 
-            const help = new Discord.MessageEmbed()
-            .setTitle("About SheepyMod")
+            const about = new Discord.MessageEmbed()
+            .setTitle("About Sheepo")
             .setAuthor("MrSuicideSheep", "https://lh3.googleusercontent.com/BXlHhxyGmyTr7w4F8tSMui6qMSYz9H-Sa-j68037r7RQyCJdX73khLuCr0fpE6qnWtMPOWlQTweItOttmjCPnGtW")
             .setColor('99ffeb')
             .setDescription("Hi! I'm Sheepo, a moderation bot developed by Killeration and Gronky, exclusively for the MrSuicideSheep Discord Server! :D \n \n There are two types of commands for this bot: ")
