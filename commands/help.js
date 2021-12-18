5b69d7ad3f4fb5d3fd407769a93d17251445725c
NotKilleration <77356224+NotKilleration@users.noreply.github.com>
2021-12-18 15:13:30 +0530

    changed embed text

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
             .setTitle("Help")
             .setAuthor("MrSuicideSheep", "https://lh3.googleusercontent.com/BXlHhxyGmyTr7w4F8tSMui6qMSYz9H-Sa-j68037r7RQyCJdX73khLuCr0fpE6qnWtMPOWlQTweItOttmjCPnGtW")
             .setColor('99ffeb')
-            .setDescription("Hey, I'm SheepyMod, a mod bot developed by Killeration and Gronky, exclusively for MrSuicideSheep Server, this bot is still in developement! :D \n \n There are two types of commands for this bot: ")
+            .setDescription("Hey, I'm SheepyMod, an all purpose bot developed by Killeration and Gronky, exclusively for MrSuicideSheep Server, this bot is still in developement! :D \n \n There are three types of commands for this bot: ")
             .addFields(
                 { name: '.helpmod', value: 'Get the Moderation commands' },
                 { name: '.helpmember', value: 'Get the commands a member can use!' },
