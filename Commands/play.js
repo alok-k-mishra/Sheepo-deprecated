f46814f2df9f05c885ae7268fdf6025909104a07
NotKilleration <mail.alokkumarmishra2004@gmail.com>
2022-09-02 17:03:35 +0530

    Create play.js

---
+++ Commands/play.js
@@ -0,0 +1,27 @@
+const { EmbedBuilder } = require("discord.js");
+const { ChatInputCommandInteraction, SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName("music")
+  .setDescription("Add a music player embed with buttons."),
+/**
+ * 
+ * @param { ChatInputCommandInteraction } interaction 
+ */
+async execute(interaction, message, client){
+        
+    const music = new EmbedBuilder()
+    .setTitle("Music Player for MrSuicideSheep Discord")
+    .setColor("5DFF00")
+    .setImage("https://external-preview.redd.it/jK3ifzXzpeZq4xUCjJfDccxU409jrLdDIhuPH7fACEA.jpg?auto=webp&s=47e117e8a7d07839e2f3872366aea64e97a04834")
+    .setDescription("Send a search term, or a link")
+
+    await interaction.reply({
+      embeds: [music] ,
+      ephemeral: false
+})
+
+
+  },
+}
