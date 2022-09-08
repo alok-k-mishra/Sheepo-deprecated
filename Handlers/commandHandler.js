096ea5a16835bf34487b79642c7733c8ca987988
NotKilleration <mail.alokkumarmishra2004@gmail.com>
2022-09-08 19:32:15 +0530

    Update commandHandler.js

--- Handlers/commandHandler.js
+++ Handlers/commandHandler.js
@@ -2,7 +2,7 @@ async function loadCommands(client) {
     const { loadFiles } = require("../Functions/fileLoader");
     const ascii = require("ascii-table");
     const table = new ascii().setHeading("Commands", "Status")
-
+    const { REST } = require('@discordjs/rest');
     await client.commands.clear();
 
     let commandsArray = [];
@@ -10,12 +10,13 @@ async function loadCommands(client) {
     const Files = await loadFiles("Commands");
 
     Files.forEach((file) =>{
+
         const command = require(file);
-        client.commands.set(command.data, command);
+        client.commands.set(command.data.name, command);
 
         commandsArray.push(command.data.toJSON());
 
-        table.addRow(command.data, "✔️");
+        table.addRow(command.data.name, "✔️");
 
 });
 
