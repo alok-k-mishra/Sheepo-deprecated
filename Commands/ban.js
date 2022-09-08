4d268194d02220b94e04296874dee67655ee1855
NotKilleration <mail.alokkumarmishra2004@gmail.com>
2022-09-08 19:16:12 +0530

    adding commands again

---
+++ Commands/ban.js
@@ -0,0 +1,43 @@
+const { EmbedBuilder, MembershipScreeningFieldType } = require("discord.js");
+const { ChatInputCommandInteraction, SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName("ban")
+  .setDescription("Ban Member.")
+  .addUserOption(option =>
+	  option.setName('target').setDescription('The member to ban')
+          .setRequired(true))
+
+  .addStringOption(option =>
+    option.setName('reason').setDescription('Reason to ban')
+          .setRequired(false))
+	
+          .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers | PermissionFlagsBits.BanMembers),
+
+/**
+ * 
+ * @param { ChatInputCommandInteraction } interaction 
+ */
+async execute(interaction, message, client){
+
+  const user = interaction.options.getUser('target');
+  const banreason = interaction.options.getString('reason')
+  guild.members.ban(user)
+  .reason(banreason);
+
+    const ban = new EmbedBuilder()
+    .setColor('FB542B')
+    .setTitle("Successfully Banned " + $(interaction.options.get('target').value)  + " :BanHammer:")
+    .setDescription("Reason: "+ banreason)
+    .setFooter("Happily moderating MrSuicideSheep server :D")
+
+    await interaction.reply({
+      embeds: [ban] ,
+      ephemeral: true
+    
+})}
+
+
+  
+}
---
+++ Commands/kick.js
@@ -0,0 +1,43 @@
+const { EmbedBuilder, MembershipScreeningFieldType } = require("discord.js");
+const { ChatInputCommandInteraction, SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName("kick")
+  .setDescription("Kick a Member.")
+  .addUserOption(option =>
+	  option.setName('target').setDescription('The member to kick')
+          .setRequired(true))
+
+  .addStringOption(option =>
+    option.setName('reason').setDescription('Reason to kick')
+          .setRequired(false))
+	
+          .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers | PermissionFlagsBits.BanMembers),
+
+/**
+ * 
+ * @param { ChatInputCommandInteraction } interaction 
+ */
+async execute(interaction, message, client){
+
+  const user = interaction.options.getUser('target');
+  const kickreason = interaction.options.getString('reason')
+  guild.members.kick(user)
+  .reason(kickreason);
+
+    const kick = new EmbedBuilder()
+    .setColor('FB542B')
+    .setTitle("Successfully Kicked " + $(interaction.options.get('target').value)  + " :BanHammer:")
+    .setDescription("Reason: "+ kickreason)
+    .setFooter("Happily moderating MrSuicideSheep server :D")
+
+    await interaction.reply({
+      embeds: [kick] ,
+      ephemeral: true
+    
+})}
+
+
+  
+}
